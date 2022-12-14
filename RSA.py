4629eb58230d6bb675e174d3ad6be7bff046dbd1
Alexander Kondratyuk <ankondratyuk_1@edu.hse.ru>
2022-12-14 16:47:27 +0300

    rsa cipher

--- RSA.py
+++ RSA.py
@@ -1,6 +1,7 @@
 import math
 
-#Введите то, что хотите зашифровать в файл text.txt
+#Введите то, что хотите зашифровать в файл plaintext.txt
+#Введите числа зашифрованного сообщения в ciphertext.txt через пробел для расшифрования
 
 def keys(p, q):
     mod = p * q
@@ -26,12 +27,15 @@ def encrypt(e, mod, phrase):
     encrypted_text = ''
     for j in range(len(encrypted_list)):
         encrypted_text += chr(encrypted_list[j])
-    return encrypted_text
+    return encrypted_list
 
-def decrypt(mod, d, encrypted_text):
+def decrypt(d, mod, phrase):
     decrypted_list = []
-    for i in range(len(encrypted_text)):
-        decrypted_list.append(((ord(encrypted_text[i]))**d) % mod)
+    arr = phrase.split(' ')
+    for i in range(len(arr)):
+        arr[i] = int(arr[i])
+    for i in range(len(arr)):
+        decrypted_list.append(((arr[i])**d) % mod)
     decrypted_text = ''
     for j in range(len(decrypted_list)):
         decrypted_text += chr(decrypted_list[j])
@@ -39,17 +43,27 @@ def decrypt(mod, d, encrypted_text):
 
 def main():
     #print('Введите текст для шифрования:')
-    with open('text.txt') as f:
-        phrase = f.read()
-    print('Введите два простых числа:')
-    p, q = map(int, input().split())
-    e, mod, d = keys(p, q)
-    print(f'Открытый ключ: ({e}, {mod})')
-    print(f'Закрытый ключ: {d}')
-    encrypted_text = encrypt(e, mod, phrase)
-    print(f'Зашифрованное сообщение:\n {encrypted_text}')
-    decrypted_text = decrypt(mod, d, encrypted_text)
-    print(f'Расшифрованное сообщение:\n {decrypted_text}')
+    print('0 - Шифрование, 1 - Расшифрование')
+    t = int(input())
+    if t == 0:
+        with open('plaintext.txt') as f:
+            phrase = f.read()
+        print('Введите два простых числа:')
+        p, q = map(int, input().split())
+        e, mod, d = keys(p, q)
+        print(f'Открытый ключ: ({e}, {mod})')
+        print(f'Закрытый ключ: {d}')
+        encrypted_list = encrypt(e, mod, phrase)
+        print(f'Зашифрованное сообщение:\n {encrypted_list}')
+    if t == 1:
+        with open('ciphertext.txt', 'r') as f:
+            phrase = f.read()
+        print('Введите ваш закрытый ключ:')
+        d = int(input())
+        print('Введите N из открытого ключа:')
+        mod = int(input())
+        decrypted_text = decrypt(d, mod, phrase) #(13, 21583) 1637
+        print(f'Расшифрованное сообщение:\n {decrypted_text}')
 
 if __name__ == '__main__':
     main()
